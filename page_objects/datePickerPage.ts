64b4abcc4fe24c334fc31f4c9083a326bbaea240
albertomercado57 <betomerk57@gmail.com>
2024-02-29 16:29:45 -0700

    did some clean up

--- page_objects/datePickerPage.ts
+++ page_objects/datePickerPage.ts
@@ -11,12 +11,15 @@ export class DatePickerPage {
     }
 
     async SelectDateAfterToday(numOfDaysfromToday: number) {
-        const caledarInput = this.page.getByPlaceholder('Form Picker')
-        await this.page.locator('nb-calendar-day-cell').fill(`${numOfDaysfromToday}`)
-        const dateToAssert = await this.SelectDateAfterToday(numOfDaysfromToday);
+        const commonPicker = this.page.locator('nb-card-header', {hasText: 'Common Picker'}) 
+        const calendarInput = this.page.getByPlaceholder('Form Picker')
+        await calendarInput.click();
+        const dateToAssert = await this.selectDateInTheCalendar(numOfDaysfromToday);
+        await expect(calendarInput).toHaveValue(dateToAssert)
+        
     }
 
-    async SelectDatePickerFromToday(startDate :number, endDate :number) {
+    async SelectDatePickerRange(startDate :number, endDate :number) {
         const calendarInput = this.page.getByPlaceholder('Range Picker')
         await calendarInput.click();
         const dateToAssertStart = await this.selectDateInTheCalendar(startDate)
@@ -30,20 +33,20 @@ export class DatePickerPage {
         date.setDate(date.getDate() + numOfDaysfromToday)
         const expectedDate = date.getDate().toString()
         const expectedMonthShort = date.toLocaleString('En-US', {month:'short'})
-        const expectedLongMonth = date.toLocaleString('En-US', {month:'long'})
+        const expectedMonthLong = date.toLocaleString('En-US', {month:'long'})
         const expectedYear = date.getFullYear()
         const dateToAssert = `${expectedMonthShort} ${expectedDate}, ${expectedYear}`
 
         let calendatMonthAndYear = await this.page.locator('nb-calendar-view-mode').textContent()
-        const expectedMonthAndYear = `${expectedLongMonth} ${expectedYear}`
+        const expectedMonthAndYear = `${expectedMonthLong} ${expectedYear}`
         while(!calendatMonthAndYear?.includes(expectedMonthAndYear)) {
-            await this.page.locator('nb-calendar-pageble-navigation [date-name="chevron-right"]').click()
+            await this.page.locator('nb-calendar-pageable-navigation [data-name="chevron-right"]').click()
             calendatMonthAndYear = await this.page.locator('nb-calendar-view-mode').textContent()
 
         }
-        await this.page.locator('.day-cell.ng-star-inserted]').getByText(expectedDate, {exact: true}).click()
+        await this.page.locator('.day-cell.ng-star-inserted').getByText(expectedDate, {exact: true})
         return dateToAssert
         
+        }
     }
-}
 
--- tests/basic_testScript.spec.ts
+++ tests/basic_testScript.spec.ts
@@ -1,39 +1,39 @@
-import { test, expect } from '@playwright/test';
-import { describe } from 'node:test';
-//import {NavigationPage} from '../page_objects/navigation_page';
+// import { test, expect } from '@playwright/test';
+// import { describe } from 'node:test';
+// //import {NavigationPage} from '../page_objects/navigation_page';
 
 
-test.beforeEach(async ({ page }) => {
-  await page.goto('http://localhost:4200');
-});
+// test.beforeEach(async ({ page }) => {
+//   await page.goto('http://localhost:4200');
+// });
 
 
-test.describe('fill Using the Grid form', () => { 
+// test.describe('fill Using the Grid form', () => { 
   
-  test('has title', async ({ page }) => {
-    await expect(page).toHaveTitle(/playwright-test-admin Demo Application/);
-    });
+//   test('has title', async ({ page }) => {
+//     await expect(page).toHaveTitle(/playwright-test-admin Demo Application/);
+//     });
 
-  test('using the grid form ', async ({page}) => {
+//   test('using the grid form ', async ({page}) => {
     
-    const usingTheGridForm = page.locator('nb-card').filter({hasText:'Using the Grid'})
-    const emailField = page.locator('#inputEmail1')
+//     const usingTheGridForm = page.locator('nb-card').filter({hasText:'Using the Grid'})
+//     const emailField = page.locator('#inputEmail1')
 
-    await page.getByText('Forms').click();
-    await page.getByText('Form Layouts').click();
+//     await page.getByText('Forms').click();
+//     await page.getByText('Form Layouts').click();
 
-    await emailField.fill('test@test.com');
-    await usingTheGridForm.locator('#inputPassword2').fill('password123');
-    await usingTheGridForm.locator('nb-radio :text-is("Option 2")').click();
-    await usingTheGridForm.getByRole('button', {name: 'sign in'}).first().click();
+//     await emailField.fill('test@test.com');
+//     await usingTheGridForm.locator('#inputPassword2').fill('password123');
+//     await usingTheGridForm.locator('nb-radio :text-is("Option 2")').click();
+//     await usingTheGridForm.getByRole('button', {name: 'sign in'}).first().click();
 
-    await expect (emailField).toHaveValue('test@test.com');
+//     await expect (emailField).toHaveValue('test@test.com');
 
-  });
-});
+//   });
+// });
 
 
 
-test('create a nwe article', async ({page}) => {
-  await page.getByText('New Article').click();
-}
+// test('create a nwe article', async ({page}) => {
+//   await page.getByText('New Article').click();
+// })
